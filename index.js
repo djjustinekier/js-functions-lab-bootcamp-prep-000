d46830de21c2a99f210d9fdc673a8df46633229f
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-05 08:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function happyHolidayTo (holiday, name){
 function holidayCountdown(holiday, days) {
   return "It\'s " + days + " days until " + holiday + "!"
 }
+
+console.log(happyHolidays())
+console.log(happyHolidaysTo("you"))
+console.log(happyHolidayTo("Independence Day", "You"))
+console.log(holidayCountdown("Mother's Day", 20))
